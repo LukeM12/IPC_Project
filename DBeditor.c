26b258d06aec980d9cb741c727f06e60faca031b
Luke Morrison <luc785@hotmail.com>
2014-12-05 16:06:22 -0500

    The database is now working well with read and write with deposit and withdraw

--- DBeditor.c
+++ DBeditor.c
@@ -15,8 +15,9 @@ struct my_msgbuf {
 	long mtype;
 	char messagetext[200];
 };
-// void ExecuteOperation(struct , int );
+char *ExecuteOperation(struct my_msgbuf,int );
 char *Withdraw(char *);
+char *Deposit(char *);
 void parseInputFile();
 int Login(char *);
 int test_user_Exists();
@@ -39,8 +40,10 @@ int main(int argc, const char *argv[]){
 		exit(1);
 	}
 	Login_Cookie = 1;
-	char * a = Withdraw("2.5");
-	printf("a = %s", a);
+	//char * a = Withdraw("2.5");
+	strcpy(receiver.messagetext, "Deposit 2");
+	char *b = ExecuteOperation(receiver, server_ID);
+	printf("\nb= %s\n", b);
 	// printf("The Database Listener is awake and waiting for Database Input\n=======================\n");
 	// /* Start the Database Data Miner  AKA DB editor*/
 	// for(;;) { 
@@ -95,9 +98,84 @@ int main(int argc, const char *argv[]){
 	// printf("Message queue was destroyed");
 	return 0;
 }
+ /************************                   Post Authentication 				 ************************  /
 
 /**
- * Pass in an amount in the form of a string to withdraw from the user 
+ * Description : This is is a lot of the meat of the salad. At this point the user is logged in and is interacting with the shell dreictly
+ *@param: receiver = the memory context that we are dealing with
+ *@param server_ID = the ID of the server that we are dealing with
+ */
+void Enter_Login(struct my_msgbuf receiver, int server_ID){
+	for(;;) { 
+		if (msgrcv(server_ID, &receiver, sizeof(receiver.messagetext), 0, 0) == -1) {
+			perror("msgrcv");
+			exit(1);
+		}
+		else {
+			//Process the request. Try and log in 
+			int len = strlen(receiver.messagetext);
+
+			printf("WE ARE IN THE SHELL\n");
+			char *msg;
+			msg = ExecuteOperation(receiver, server_ID);
+			//Now what we want to do is paste that and send it back to the user 
+			//just return a char here ideally with their currenty balance and send it back
+
+
+			len = strlen(receiver.messagetext); // We meed to reset our length after 
+			if (msgsnd(server_ID, &receiver,len+1, 0) == -1){
+				printf("Message was not sent\n");
+				perror("msgsnd");
+			}
+			else {
+				printf("Editor tries to send message back to server\n");
+			} 
+		}
+		printf("DBserver SAYS: \"%s\"\n", receiver.messagetext);
+	}
+}
+/**
+ * Description : This is the engine that takes an operating and dissects it to find out what it should do, and then sends it to get done
+ *@param: receiver = the memory context that we are dealing with
+ *@param server_ID = the ID of the server that we are dealing with
+ */
+char *ExecuteOperation(struct my_msgbuf receiver, int server_ID){
+	char copy[200];
+	strcpy( copy, receiver.messagetext);
+	char* token = strtok(receiver.messagetext, " ");
+	int count_fields = 0;
+	float value;
+	char op[10];
+	char *msg;
+	char ret[100];
+	while (token) {
+		printf("We are in the main lop");
+	    //printf("token: %s\n", token);
+		if (strcmp(token, "Withdraw") == 0){
+			token = strtok(NULL, " ");
+			value = atof(token);
+			printf("token = %f\n", value);
+			strcpy(receiver.messagetext, copy );	
+			msg = Withdraw(token);
+			strcpy(ret, msg);
+		}
+		if (strcmp(token, "Deposit") == 0)
+		{
+			token = strtok(NULL, " ");
+			value = atof(token);
+			printf("token = %f\n", value);
+			strcpy(receiver.messagetext, copy );	
+			msg = Deposit(token);
+			strcpy(ret, msg);
+		}
+	    token = strtok(NULL, " ");
+	    count_fields++;
+	}
+	return ret;
+}
+
+/**
+ * Description : Pass in an amount in the form of a string to withdraw from the user 
  * @param val_asString: the value that is being passe in
  */
 char* Withdraw(char * val_asString){
@@ -198,7 +276,9 @@ char* Withdraw(char * val_asString){
 		}
 	//We have read the whole string just output the rest of the DB
 	if (processed_User_Flag > 0 && FIRST_PROCESS == 0){
-		fprintf(newDB, "%s", backup_String);
+		if (strlen(backup_String > 8){
+		 	fprintf(newDB, "%s", backup_String);
+		}
 	}
 	//Flips the bit that tells us - we already wrote the current user
 	if (FIRST_PROCESS == 1) {
@@ -210,10 +290,167 @@ char* Withdraw(char * val_asString){
 	}
 	fclose(newDB);
 	fclose(oldDB);
+    int status = remove("database.txt");
 
+	if( status == 0 )
+	  printf(" file deleted successfully.\n");
+	else
+	{
+	  printf("Unable to delete the file\n");
+	  perror("Error");
+	}
+	/* Clean up the files */
+   char oldname[] = "database2.txt";
+   char newname[] = "database.txt";
+   
+   int test = rename(oldname, newname);
 
+   if(test == 0) 
+   {
+      printf("File renamed successfully");
+   }
+   else 
+   {
+      printf("Error: unable to rename the file");
+   }
 	return ret;
 }
+
+/**
+ * Description : Pass in an amount in the form of a string to withdraw from the user 
+ * @param val_asString: the value that is being passe in
+ */
+char* Deposit(char * val_asString){
+	float val = atof(val_asString);
+	printf("token = %f\n", val);
+	int AccountNum = Login_Cookie;
+	printf("Incoming Val = %f", val);
+    char *localString;
+    size_t len = 0;
+    int bytes_read;
+    char *token2;
+    const char s[2] = ",";
+    char copy[100];
+    char backup_String[100];
+   	//This will be the message to send back to the ATM
+    char ret[100];
+    memset(ret, '\0', sizeof(ret));
+
+    /** 
+    * File Weaving technique. Open 2 files. Write updates file to 2nd one. Delete 1st one, and then 
+    * rename 2nd one to have name as 1st one. OS inherently protects filelock, and the benefit is 
+    * protection against memory overflow 
+    **/
+    FILE* oldDB = fopen("database.txt","r");
+    if (oldDB == NULL){
+    	printf("ERROR: FILE OPEN\n");
+    	exit(1);
+    }
+	FILE* newDB = fopen("database2.txt","w");
+    if (newDB == NULL){
+		printf("ERROR: FILE OPEN\n");
+		exit(1);
+    }
+    /* Our counters for the fileLooping */
+    int count_entries = 0;
+    int count_fields = 0;
+
+    /* Avoid tokenizing for unimportant entries */
+    int processed_User_Flag = 0;
+    int FIRST_PROCESS = 0;
+    /* Read the entire file */
+    while (bytes_read != -1) { 
+    	int been_written = 0;
+        bytes_read = getline(&localString, &len, oldDB);
+
+        memset(backup_String, '\0', sizeof(backup_String));
+        strcpy(backup_String, localString);    
+        char* token = strtok(localString, ",");
+
+        /* Parse the string */
+		while (token && ! processed_User_Flag ) {
+		    long int inputAccountNum = strtol(token, NULL, 10);
+		    /* The user already autheticated, we do not need to worry about the PIN again */
+		    if (count_fields==0 && inputAccountNum == AccountNum){
+		    	//clear out our memory 
+		    	memset(copy, '\0', sizeof(copy));
+
+		    	//grab the user ID, put it in the buffer
+		    	strcat(copy, token);
+		    	strcat(copy, ",");
+		    	//Grab the PIN, append to buffer
+		    	token = strtok(NULL, ",");
+		    	strcat(copy, token);
+		    	strcat(copy, ",");
+
+		    	//Grab The Balance
+		    	token = strtok(NULL, ",");
+		    	float DBval = atof(token);
+		    	float effective = DBval + val;
+				sprintf(ret, "%.2f Deposited. Balance=%.2f", val, effective);
+
+		    	char dummy[100];
+		    	//printf("In the database it is %s DBval=%f\nval=%f\neffective=%f\n" , copy, DBval, val, effective);
+		    	if (processed_User_Flag == 0){
+					fprintf(newDB, "%s%.2f\n", copy,effective);
+					//To not have to loop this each time
+					processed_User_Flag = 1;
+					//To not rewrite this line afterwards
+					FIRST_PROCESS = 1;
+				}
+				
+		    }
+		    else {
+		    	/* If it not the user just write out to the second file */
+				// fprintf(newDB, "%s", backup_String);
+		    }
+		    token = strtok(NULL, ",");
+		    count_fields++;
+		}
+		//We have read the whole string just output the rest of the DB
+		if (processed_User_Flag > 0 && FIRST_PROCESS == 0){
+			if (strlen(backup_String) > 8){
+				fprintf(newDB, "%s", backup_String);
+			}
+		}
+		//Flips the bit that tells us - we already wrote the current user
+		if (FIRST_PROCESS == 1) {
+			FIRST_PROCESS = 0;
+		}
+
+		count_fields = 0;
+		count_entries++;
+	}
+	fclose(newDB);
+	fclose(oldDB);
+    int status = remove("database.txt");
+
+	if( status == 0 )
+	  printf(" file deleted successfully.\n");
+	else
+	{
+	  printf("Unable to delete the file\n");
+	  perror("Error");
+	}
+	/* Clean up the files */
+   char oldname[] = "database2.txt";
+   char newname[] = "database.txt";
+   
+   int test = rename(oldname, newname);
+
+   if(test == 0) 
+   {
+      printf("File renamed successfully");
+   }
+   else 
+   {
+      printf("Error: unable to rename the file");
+   }
+	return ret;
+}
+
+ /************************                   /End Post Authentication		 ************************  /
+ /************************                   Authentication 				 ************************  /
 /**
  * Description : The log in method to check to see if the user exists
  * pass in the user account like this ACCOUNT PIN 
@@ -245,7 +482,7 @@ int Login(char *a){
 	return user_Exists(Account, PIN);
 }
 /** 
- * Verify if user Exists under this PIN and Account Number
+ * Description : Verify if user Exists under this PIN and Account Number
  * @param Account Num - the user Account number. This will be cached locally upon login
  * @param PIN - authenticates the user based on content of database.txt
  * return: 1 if success, 0 of not successful
@@ -299,75 +536,6 @@ int test_user_Exists(){
 	}
      return 0;
 }
-
-
-void Enter_Login(struct my_msgbuf receiver, int server_ID){
-	for(;;) { 
-		if (msgrcv(server_ID, &receiver, sizeof(receiver.messagetext), 0, 0) == -1) {
-			perror("msgrcv");
-			exit(1);
-		}
-		else {
-			//Process the request. Try and log in 
-			int len = strlen(receiver.messagetext);
-
-			printf("WE ARE IN THE SHELL\n");
-			int a = ExecuteOperation(receiver, server_ID);
-			printf("Left Execute Operation\n");
-
-			//just return a char here ideally with their currenty balance and send it back
-
-
-			len = strlen(receiver.messagetext); // We meed to reset our length after 
-			if (msgsnd(server_ID, &receiver,len+1, 0) == -1){
-				printf("Message was not sent\n");
-				perror("msgsnd");
-			}
-			else {
-				printf("Editor tries to send message back to server\n");
-			} 
-		}
-		printf("DBserver SAYS: \"%s\"\n", receiver.messagetext);
-	}
-}
-void ExecuteOperation(struct my_msgbuf receiver, int server_ID){
-
-	printf("INside ex Your string = %s\n", receiver.messagetext);
-	char copy[200];
-	strcpy( copy, receiver.messagetext);
-	char* token = strtok(receiver.messagetext, " ");
-	int count_fields = 0;
-	float value;
-	char op[10];
-	while (token) {
-	    //printf("token: %s\n", token);
-		if (strcmp(token, "Withdraw") == 0){
-			//Get the other one 
-			//printf("User can only withdraw in Dollars\n");
-			char a[200];
-			token = strtok(NULL, " ");
-			value = atof(token);
-			//strcpy(a,token;
-			printf("token = %f\n", value);
-			//Call Server to WithDraw because its a decent command
-			strcpy(receiver.messagetext, copy );
-			
-			Withdraw(token);
-
-		}
-		if (strcmp(token, "Deposit") == 0)
-		{
-			token = strtok(NULL, " ");
-			value = atof(token);
-			printf("token = %f\n", value);
-			//Call Server to Deposit
-		}
-	    token = strtok(NULL, " ");
-	    count_fields++;
-	}
-	return 1;
-}
-
 /**
  * Description : This checks to see if the end user exists  
  * @param: the User Account Number
